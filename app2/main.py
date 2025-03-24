1c81ca1da07b2f8725065c4b2c2902da2537e1d1
MatveySviadysh <mkomp06@gmail.com>
2025-03-24 22:58:14 +0300

    add metrics

--- app2/main.py
+++ app2/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI(root_path="/server2")
+
+ITEMS_COUNTER = Counter("items_requests", "Number of requests to items endpoint")
 
 
 @app.get("/")
@@ -10,4 +14,10 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    ITEMS_COUNTER.inc()
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/metrics")
+def metrics():
+    return PlainTextResponse(generate_latest())
