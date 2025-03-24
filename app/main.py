ad12699cd267caf679a55274f2f798c64f5bd2b8
MatveySviadysh <mkomp06@gmail.com>
2025-03-24 23:09:14 +0300

    settings cors & logging

--- app/main.py
+++ app/main.py
@@ -1,23 +1,41 @@
 from fastapi import FastAPI
-from prometheus_client import Counter, generate_latest
 from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from prometheus_client import Counter, generate_latest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI(root_path="/server1")
 
-ITEMS_COUNTER = Counter("items_requests", "Number of requests to items endpoint")
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+ITEMS_COUNTER = Counter("items_requests", "Number of requests to items endpoint")
 
 @app.get("/")
 def read_root():
+    logger.info("Root endpoint called")
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    # Увеличиваем счетчик для метрик
+    logger.info(f"Items endpoint called with item_id: {item_id} and query: {q}")
     ITEMS_COUNTER.inc()
     return {"item_id": item_id, "q": q}
 
 @app.get("/metrics")
 def metrics():
-    # Возвращаем метрики в формате для Prometheus
+    logger.info("Metrics endpoint called")
     return PlainTextResponse(generate_latest())
